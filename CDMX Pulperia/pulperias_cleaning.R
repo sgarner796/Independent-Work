0c6cd90fa4e1ba669a67d2201b136c65c9e5683b
sgarner796 <sgarner796@gmail.com>
2025-02-08 09:56:03 -0600

    short update c
    created binary variables to measure connectivity

--- CDMX Pulperia/pulperias_cleaning.R
+++ CDMX Pulperia/pulperias_cleaning.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(sf)
 
 #Set working directory
-setwd("M:/p/GitHub/Independent Work/CDMX Pulperia")
+setwd("C:/Users/steph/Documents/GitHub/Independent-Work/CDMX Pulperia")
 
 pulperia <- read.xlsx("pulperias.xlsx", sheet = 2)
 empleos <- read.xlsx("empleo_09.xlsx")
@@ -24,12 +24,15 @@ empleos <- empleos %>%
 not_needed <- c("cve_entidad","desc_entidad","cve_municipio","id_indicador")
 
 empleos <- empleos %>% 
-  select(-all_of(not_needed)) %>% 
-  filter(indicador == " Porcentaje de la población de 12 años y más económicamente activa ocupada")
+  select(-all_of(not_needed)) 
 
 PEA <- empleos %>% 
-  select(desc_municipio,indicador,`2020`) %>% 
+select(desc_municipio,indicador,`2020`) %>% 
   filter(indicador == "Porcentaje de población de 12 años y más económicamente activa")
+
+PEA <- PEA  %>% 
+  rename(PEA = `2020`) %>% 
+  select(desc_municipio, PEA)
 
 ## comercio
 
@@ -40,5 +43,22 @@ comercio_clean <- comercio[-1, ]
 comercio_trim <- comercio_clean %>% 
   slice(-1:-20)
 
+comercio_trim<- comercio_trim %>% 
+  select(desc_municipio,indicador,`2008`,`2018`)
 
+comercio_trim <- comercio_trim %>% 
+  filter(indicador == "Total de ingresos por suministro de bienes y servicios. Gran sector 43-46. Comercio") %>% 
+  rename(ingresos = `2008`)
 
+comercio_trim <- comercio_trim %>% 
+  select(desc_municipio,ingresos)
+
+### Creacion de variables pulpe
+
+pulperia <- pulperia %>%
+  mutate(
+    Telefono_binary = ifelse(!is.na(`Telefono`) & `Telefono` != "", 1, 0),
+    Correo_binary = ifelse(!is.na(`Correo.Electrónico`) & `Correo.Electrónico` != "", 1, 0),
+    Pagina_binary = ifelse(!is.na(`Págiona.web`) & `Págiona.web` != "", 1, 0),
+    Count_non_missing_percentage = (`Telefono_binary` + `Correo_binary` + `Pagina_binary`) / 3 * 100
+  )
